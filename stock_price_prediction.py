5442805f24e04bda70d3e4261c6cb99029045f5c
Tony <46513807+wutonytt@users.noreply.github.com>
2021-04-26 19:36:07 +0800

    Add files via upload

---
+++ stock_price_prediction.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tensorflow.python.keras.engine.training import _is_scalar
+import pandas_datareader as web
+import datetime
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+
+# Load Data
+# company = 'FB'  # ticker symbol
+company = input('Input the Ticker Symbol of a Company: ')
+
+start = datetime.datetime(2012, 1, 1)
+end = datetime.datetime(2020, 1, 1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+# Preprocess Data
+scalar = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scalar.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+X_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    X_train.append(scaled_data[x - prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+# Build the Model
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(X_train, y_train, epochs=25, batch_size=32)
+
+#### Test the Model Accuracy on Existing Data ####
+test_start = datetime.datetime(2020, 1, 1)
+test_end = datetime.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scalar.transform(model_inputs)
+
+# Make Prediction on Test Data
+X_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    X_test.append(model_inputs[x-prediction_days:x, 0])
+
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+predicted_prices = model.predict(X_test)
+predicted_prices = scalar.inverse_transform(predicted_prices)
+
+# Plot the Test Predictions
+plt.plot(actual_prices, color='black', label=f'Actual {company} Price')
+plt.plot(predicted_prices, color='green', label=f'Predicted {company} Price')
+plt.title(f'{company} Share Price')
+plt.xlabel('Time')
+plt.ylabel(f'{company} Share Price')
+plt.legend()
+plt.show()
+
+# Predict Next Day
+real_data = [model_inputs[len(model_inputs) - prediction_days:len(model_inputs), 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data,(real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scalar.inverse_transform(prediction)
+print(f'Prediction: {prediction}')
